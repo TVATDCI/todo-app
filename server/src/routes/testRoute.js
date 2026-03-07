3cf816df31c7fa28b48e35a5337dd4eae54c4e4d
TVATDCI <tvatdci@gmail.com>
2026-03-07 19:48:08 +0100

    update: Scaffolding

--- server/src/routes/testRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getTestController } from '../controllers/testController.js';
-
-const router = express.Router();
-
-// Example route handler
-router.get('/example', getTestController); // GET /api/example
-
-export default router;
