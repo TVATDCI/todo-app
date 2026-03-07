17ab47514ce343461e898924a92666f056a439e5
TVATDCI <tvatdci@gmail.com>
2026-03-07 20:13:56 +0100

    update: remove redundant

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -27,9 +27,6 @@ const userSchema = new mongoose.Schema(
   },
 );
 
-// Index for faster queries
-userSchema.index({ email: 1 });
-
 const User = mongoose.model("User", userSchema);
 
 export default User;
